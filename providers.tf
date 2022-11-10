2526107a14677a6178cd45690b9bd113f1546a40
brbva <brbv.a@protonmail.com>
2022-11-10 19:08:18 +0800

    fix providers

--- providers.tf
+++ providers.tf
@@ -7,8 +7,8 @@ terraform {
   }
 }
 
-provider "aws" {
-  #region                   = "us-west-2"
-  #shared_credentials_files = ["/mnt/c/Users/Bjay/.aws/credentials"]
-  profile                  = "vscode"
-}
+# provider "aws" {
+#   region                   = "us-west-2"
+#   shared_credentials_files = ["/mnt/c/Users/Bjay/.aws/credentials"]
+#   profile                  = "vscode"
+# }
